b338af7e701fae152d5a6c0d6d3275f19305bda1
AmjadKhalil <akk008@bucknell.edu>
2018-07-08 19:13:57 -0400

    add Orders page

--- src/test/java/pages/OrdersPage.java
+++ src/test/java/pages/OrdersPage.java
@@ -1,6 +1,8 @@
 package pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class OrdersPage {
@@ -8,6 +10,45 @@ public class OrdersPage {
 	public OrdersPage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_txtQuantity")
+	public WebElement quantity;  //clear().sendKeys
+	
+//	@FindBy(id="ctl00_MainContent_fmwOrder_txtDiscount")
+//	public WebElement discount;
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_txtName")
+	public WebElement cusName;
+
+	@FindBy(id="ctl00_MainContent_fmwOrder_TextBox2")
+	public WebElement street;
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_TextBox3")
+	public WebElement city;
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_TextBox4")
+	public WebElement state;
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_TextBox5")
+	public WebElement zip;
+	
+	// "//input[@type='radio'][1]"
+	@FindBy(xpath="//input[@type='radio'][1]")
+	public WebElement cardType;
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_TextBox6")
+	public WebElement cardNr;
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_TextBox1")
+	public WebElement expDate;
+	
+	@FindBy(id="ctl00_MainContent_fmwOrder_InsertButton")
+	public WebElement processBtn; // click()
+	
+	
+	
+	
+	
 	
 	
 	
